a3585f733482d3f5b0dfcd3b461cb716642280db
Setya Budi Pratama <setyabudipratam@gmail.com>
2024-10-28 20:42:14 +0700

    Update about.js

--- about.js
+++ about.js
@@ -15,3 +15,35 @@ const shop = document.querySelector('.nav-item:nth-child(3) .nav-link');
 shop.addEventListener('click', () => {
     window.location.href = "shop.html";
 });
+
+
+// rubah footer
+const footer = document.querySelector('footer');
+Object.assign(footer.style, {
+    justifyContent: 'space-around',
+});
+
+const h5Footer = document.querySelector('footer h5');
+h5Footer.style.fontSize = '1rem';
+
+const spanFooter = document.querySelectorAll('footer span');
+spanFooter.forEach(span => {
+    span.style.fontSize = '0.8rem';
+});
+
+function applyResponsiveStyles () {
+    if (window.innerWidth < 767) {
+        Object.assign(footer.style, {
+            justifyContent: 'center',
+            gap: '0',
+        });
+        h5Footer.style.fontSize = '0.6rem';
+        spanFooter.forEach(span => {
+            span.style.fontSize = '0.6rem';
+        });
+    };
+};
+
+// saat layar mengubah ukuran
+applyResponsiveStyles();
+window.addEventListener('resize', applyResponsiveStyles);
